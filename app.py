a30f1d25a297cddc4f22e6b26b65da4a26590399
rafnaves <rafaelnavesdev@gmail.com>
2025-04-22 20:13:08 -0300

    Rotas e Modelagem inicial

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+from repository.database import db
+from db_models.payment import Payment
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+
+db.init_app(app)
+
+
+
+@app.route('/payments/pix', methods=['POST'])
+def create_payment_pix():
+    data = request.get_json()
+
+    # Validações
+    if 'value' not in data:
+        return jsonify({"message": "Valor Invalido"}), 400
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({"message": "O pagamento foi criado",
+                    "payment": new_payment.to_dict()})
+
+@app.route('/payments/pix/confirmation', methods=['POST'])
+def pix_confirmation():
+    return jsonify({"message": "O pagamento foi confirmado"})
+
+
+@app.route('/payments/pix/<int:payment_id>', methods=['GET'])
+def payment_pix_page(payment_id):
+    return 'pagamento pix'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+

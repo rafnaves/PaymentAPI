6847ab10db74a51dc32104cf42762ace5e8ad7db
rafnaves <rafaelnavesdev@gmail.com>
2025-04-24 19:19:33 -0300

    html & css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, render_template
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
@@ -16,30 +16,34 @@ db.init_app(app)
 def create_payment_pix():
     data = request.get_json()
 
-    # Validações
     if 'value' not in data:
         return jsonify({"message": "Valor Invalido"}), 400
-    
+
     expiration_date = datetime.now() + timedelta(minutes=30)
 
     new_payment = Payment(value=data['value'], expiration_date=expiration_date)
 
-
     pix_obj = Pix()
     data_payment_pix = pix_obj.create_payment()
     new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
-    new_payment.qr_code = data_payment_pix["qr_code_path"]
+
+    qr_code_path = data_payment_pix["qr_code_path"]
+    qr_code_filename = qr_code_path.split("/")[-1]
+    new_payment.qr_code = qr_code_filename
+
 
     db.session.add(new_payment)
     db.session.commit()
 
-    return jsonify({"message": "O pagamento foi criado",
-                    "payment": new_payment.to_dict()})
+    return jsonify({
+        "message": "O pagamento foi criado",
+        "payment": new_payment.to_dict()
+    })
 
 
 @app.route('/payments/pix/qr_code/<file_name>', methods=["GET"])
 def get_image(file_name):
-    return send_file(f"static/img/{file_name}.png", mimetype='image/png')
+    return send_file(f"static/img/{file_name}", mimetype='image/png')
 
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
@@ -49,7 +53,14 @@ def pix_confirmation():
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
-    return 'pagamento pix'
+    payment = Payment.query.get(payment_id)
+
+
+    return render_template('payment.html', 
+                           payment_id=payment.id, 
+                           value=payment.value, 
+                           host="http://127.0.0.1:5000",
+                           qr_code= payment.qr_code)
 
 if __name__ == '__main__':
     app.run(debug=True)

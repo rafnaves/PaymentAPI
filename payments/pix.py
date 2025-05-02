57d04dd567fc967ec4dc110b226912898b85aef3
rafnaves <rafaelnavesdev@gmail.com>
2025-05-02 14:15:48 -0300

    final

--- payments/pix.py
+++ payments/pix.py
@@ -5,7 +5,7 @@ class Pix:
     def __init__(self):
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # Criação do pagamento na instituição financeira
 
         bank_payment_id = str(uuid.uuid4())
@@ -16,7 +16,7 @@ class Pix:
         hash_payment = f'hash_payment_{bank_payment_id}'
         img = qrcode.make('hash_payment')
         type(img)
-        img.save(f"static/img/qr_code_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_{bank_payment_id}.png")
 
         return {
             "bank_payment_id": str(bank_payment_id),
